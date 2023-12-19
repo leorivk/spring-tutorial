d50d279d5ae1eb73fca5c89ef9cbf80d1c016259
ivk <leorivk@gmail.com>
2023-12-20 07:57:47 +0900

    Apply dependency injection (OOP)

--- src/main/java/study/core/MemberApp.java
+++ src/main/java/study/core/MemberApp.java
@@ -3,13 +3,15 @@ package study.core;
 import study.core.member.Grade;
 import study.core.member.Member;
 import study.core.member.MemberService;
-import study.core.member.MemberServiceImpl;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "memberA", Grade.VIP);
+
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
--- src/main/java/study/core/OrderApp.java
+++ src/main/java/study/core/OrderApp.java
@@ -3,16 +3,16 @@ package study.core;
 import study.core.member.Grade;
 import study.core.member.Member;
 import study.core.member.MemberService;
-import study.core.member.MemberServiceImpl;
 import study.core.order.Order;
 import study.core.order.OrderService;
-import study.core.order.OrderServiceImpl;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
