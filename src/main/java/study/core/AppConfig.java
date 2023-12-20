99e7c9a9c3239d18826e8ab9ccf7a09b4c409243
ivk <leorivk@gmail.com>
2023-12-20 09:18:41 +0900

    Introduce Spring (Spring Container, Spring Bean)

--- src/main/java/study/core/AppConfig.java
+++ src/main/java/study/core/AppConfig.java
@@ -1,5 +1,7 @@
 package study.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import study.core.discount.DiscountPolicy;
 import study.core.discount.RateDiscountPolicy;
 import study.core.member.MemberService;
@@ -8,20 +10,25 @@ import study.core.member.MemoryMemberRepository;
 import study.core.order.OrderService;
 import study.core.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
