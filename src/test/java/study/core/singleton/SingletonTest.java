c5195914dbd0158ae79ca35cf43b1d2184cb65a0
ivk <leorivk@gmail.com>
2023-12-21 07:58:47 +0900

    Spring DI container (Singleton)

--- src/test/java/study/core/singleton/SingletonTest.java
+++ src/test/java/study/core/singleton/SingletonTest.java
@@ -3,6 +3,8 @@ package study.core.singleton;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import study.core.AppConfig;
 import study.core.member.MemberService;
 
@@ -39,5 +41,20 @@ public class SingletonTest {
         System.out.println("instance2 = " + instance2);
 
         assertThat(instance1).isSameAs(instance2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // 같은 객체 반환
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
