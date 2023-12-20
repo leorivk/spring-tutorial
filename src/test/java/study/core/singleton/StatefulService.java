86bbb671a366bf37b94210a574e4ffc9ca8ee69d
ivk <leorivk@gmail.com>
2023-12-21 08:11:02 +0900

    Problem in Singleton

---
+++ src/test/java/study/core/singleton/StatefulService.java
@@ -0,0 +1,17 @@
+package study.core.singleton;
+
+public class StatefulService {
+
+    private int price; // 상태를 유지하는 필드 (공유되는 필드)
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void order(String name, int price) {
+        System.out.println("name = " + name + ", price = " + price);
+        this.price = price; // 여기가 문제!
+
+
+    }
+}
